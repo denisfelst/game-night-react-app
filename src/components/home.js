d83f0e97721d29934ba9e3b39c3d3f8a14386f50
denisfelst <dfg@demodern.de>
2022-10-12 15:45:36 +0200

    styling home

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,15 @@
-import { Link, Router } from "react-router-dom";
 import logo from "./../assets/pizza.png";
 import NextBtn from "./nextBtn";
 
 function Home({ current, changeCurrent }) {
   return (
     <div className="App">
-      <h1>EPIC GAME</h1>
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Listos?</h2>
-      <NextBtn current={current} changeCurrent={() => changeCurrent()} />
+      <div className="App-home">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Game Night La Villa</h1>
+        <h2>¿Listos?</h2>
+        <NextBtn current={current} changeCurrent={() => changeCurrent()} />
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdKxZgsPQxQGG5uB70q0OTPoD3LVmbkiI",
+  authDomain: "game-night-villa.firebaseapp.com",
+  projectId: "game-night-villa",
+  storageBucket: "game-night-villa.appspot.com",
+  messagingSenderId: "964266546393",
+  appId: "1:964266546393:web:fe3632eefb7cf182a05ac8",
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
